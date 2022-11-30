c6191f9f2b50f7c4ccf141a41b73fd4089a77380
Dragindor <hhristov2001@googlemail.com>
2022-11-30 19:08:53 +0200

    created the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllGamesComponent } from './all-games/all-games.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostsComponent } from './posts/posts.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllGamesComponent,
+    ContactComponent,
+    PostsComponent,
+    GameDetailsComponent
   ],
   imports: [
     BrowserModule
