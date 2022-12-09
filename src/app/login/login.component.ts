e74331bc851f108b749ec184d1d02286393dbfc7
Dragindor <hhristov2001@googlemail.com>
2022-12-09 18:43:15 +0200

    Login works

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Login } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  handleFormSubmit(value: {username: string; email: string; password: string;}): void{
+    console.log(value);
+    Login(value);
+  }
 
 }
+
