c5d0fb6fed456d076f6c2c3462d2409d6bdfef86
Dragindor <hhristov2001@googlemail.com>
2022-12-10 18:59:43 +0200

    changed register to work with firebase auth

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { Register} from '../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-  handleFormSubmit(value: {username: string; email: string; password: string;}): void{
-    console.log(value);
-    Register(value);
-  }
+export class RegisterComponent implements OnInit{
+  constructor(
+    public authService: AuthService
+  ) { }
+  ngOnInit() { }
 }
