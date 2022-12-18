a6d9ba7fff402f123f3fbf2426c64670382b8f41
Dragindor <hhristov2001@googlemail.com>
2022-12-18 19:59:49 +0200

    fixed routing to not load home twice

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -10,16 +10,17 @@ import { ContactComponent } from './contact/contact.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
 
 const routes: Routes = [
-    { path: '', component: AppComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'all-games', component: AllGamesComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'game-details', component: GameDetailsComponent },
-
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/'
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'all-games', component: AllGamesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'game-details', component: GameDetailsComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
