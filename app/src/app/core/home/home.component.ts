33122c5f8743f7731e0b24c4609de8af534274d4
Dragindor <hhristov2001@googlemail.com>
2022-12-21 20:54:29 +0200

    change the home page works

--- app/src/app/core/home/home.component.ts
+++ app/src/app/core/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 import { AuthService } from 'src/app/auth/auth.service';
+import { IGame } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +10,20 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class HomeComponent {
 
+  gameList: IGame[] | null = null;
+  errorFetcingData = false;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.loadGames().subscribe({
+      next: (value) => {
+        this.gameList = value.reverse().slice(0,2);
+      },
+      error: (err) => {
+        this.errorFetcingData = true;
+        console.error(err);
+      }
+    });
+  }
 }
