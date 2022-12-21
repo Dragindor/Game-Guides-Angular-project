9ecaff9d2f4f9907618ea52809b0001d4b7a3bee
Dragindor <hhristov2001@googlemail.com>
2022-12-21 20:53:19 +0200

    moved and worked on the dashboard component

---
+++ app/src/app/auth/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
+import { IGame } from 'src/app/shared/interfaces';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  games !: IGame[];
+  userId : string | undefined;
+  userName: string | undefined;
+  isEmpty !: boolean;
+  
+  constructor(private apiService: ApiService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.apiService.loadGames().subscribe({
+      next:(value) =>{
+        this.games = value
+        this.userId = this.authService.user?._id.toString()
+        this.games = this.games.filter(x => x._ownerId._id.toString() == this.userId)
+        this.userName =this.games[0]._ownerId.username;
+        if (this.games.length <=0) {
+          this.isEmpty = true;
+        } else {
+          this.isEmpty = false;
+        }
+      },
+      error: (err) => {console.error(err)}
+    })}
+  }
+
