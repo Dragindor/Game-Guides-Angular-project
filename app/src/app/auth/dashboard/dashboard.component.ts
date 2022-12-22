15b324d4ff7a5a1404ac532c50b8ccd319160849
Dragindor <hhristov2001@googlemail.com>
2022-12-22 17:01:13 +0200

    added delete button and changed how the page looks

--- app/src/app/auth/dashboard/dashboard.component.ts
+++ app/src/app/auth/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { GameService } from 'src/app/game/game.service';
 import { IGame } from 'src/app/shared/interfaces';
 import { AuthService } from '../auth.service';
 
@@ -15,14 +16,23 @@ export class DashboardComponent implements OnInit {
   userName: string | undefined;
   isEmpty !: boolean;
   
-  constructor(private apiService: ApiService, private authService: AuthService) { }
+  constructor(private apiService: ApiService, private authService: AuthService, private gameService: GameService, private router: Router,) {} 
 
+  delete(gameId:string){
+    this.gameService.deleteGame(gameId).subscribe({
+      next: ()=>{
+        this.router.navigate(['/games/all-games'])     
+      },
+      error: (err) => {console.error(err)}
+      
+    })}
+    
   ngOnInit(): void {
     this.apiService.loadGames().subscribe({
       next:(value) =>{
         this.games = value
         this.userId = this.authService.user?._id.toString()
-        this.games = this.games.filter(x => x._ownerId._id.toString() == this.userId)
+        this.games = this.games.filter(x => x._ownerId._id.toString() == this.userId).reverse()
         this.userName =this.games[0]._ownerId.username;
         if (this.games.length <=0) {
           this.isEmpty = true;
